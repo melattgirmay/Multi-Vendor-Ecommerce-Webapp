c36584976cf75ae60ec412f999d3540b0456bcc4
Melat Hailu <melatgirmay20@gmail.com>
2025-01-11 14:38:54 +0300

    Handle Logout

--- frontend/src/components/VendorHeader.js
+++ frontend/src/components/VendorHeader.js
@@ -6,16 +6,10 @@ import { FaBars } from "react-icons/fa";
 
 const VendorHeader = () => {
   const navigate = useNavigate();
-
-  const handleLogout = () => {
-    localStorage.removeItem("vendorToken");
-    navigate("/vendor-login");
-  };
-
   return (
     <div>
       {/* Sticky Header */}
-      <div className="fixed top-0 left-0 z-10 bg-customBlue shadow-md w-full h-16 flex items-center justify-between px-6 md:px-10 text-white">
+      <div className=" bg-customBlue shadow-md w-full h-16 flex items-center justify-between px-6 md:px-10 text-white">
         {/* Logo or Branding */}
         <div className="text-xl font-semibold cursor-pointer">Vendor Panel</div>
 
@@ -28,12 +22,6 @@ const VendorHeader = () => {
             <CgProfile size={28} />
             <span className="hidden md:block">Dashboard</span>
           </Link>
-          <button
-            onClick={handleLogout}
-            className="bg-red-600 text-white px-4 py-2 rounded-lg hover:bg-red-700 transition duration-200"
-          >
-            Logout
-          </button>
         </div>
       </div>
     </div>
